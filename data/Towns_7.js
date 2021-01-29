e70cf52ad1278cf9fce495902385cd58caf704cc
teamdhaksha <63536148+teamdhaksha@users.noreply.github.com>
2021-01-29 11:22:19 +0530

    Add files via upload

---
+++ data/Towns_7.js
@@ -0,0 +1,13 @@
+var json_Towns_7 = {
+"type": "FeatureCollection",
+"name": "Towns_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAME": "Salem" }, "geometry": { "type": "Point", "coordinates": [ 78.15120058128376, 11.649465585672139, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Yercaud" }, "geometry": { "type": "Point", "coordinates": [ 78.206491143748707, 11.77504257415729, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Mettur" }, "geometry": { "type": "Point", "coordinates": [ 77.797165999155524, 11.79213434331978, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Idappadi" }, "geometry": { "type": "Point", "coordinates": [ 77.835777649395411, 11.58338858602729, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Attur" }, "geometry": { "type": "Point", "coordinates": [ 78.59427588418184, 11.594705162740549, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Thammampatti" }, "geometry": { "type": "Point", "coordinates": [ 78.489425177972379, 11.440369828058961, -999999.0 ] } }
+]
+}
